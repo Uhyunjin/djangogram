8fd37ca0895b12af83e2af3ecb03f571158294b2
hyeonjin <uhyeonjin0619@gmail.com>
2022-08-05 17:58:37 +0900

    test-error

--- djangogram/djangogram/posts/test/test_view.py
+++ djangogram/djangogram/posts/test/test_view.py
@@ -1,7 +1,14 @@
 from django.test import TestCase
 from django.urls import reverse
+from django.contrib.auth import get_user_model
+from django.core.files.uploadedfile import SimpleUploadedFile
 
 class TestPost(TestCase):
+    def setUp(self):
+        User = get_user_model()
+        self.user = User.objects.create_user(
+            username='jungo', email='jungo@gmail.com', password='1234'
+        )
 
     def test_get_posts_page(self):
         url = reverse('posts:post_create')
@@ -9,3 +16,28 @@ class TestPost(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'posts/post_create.html')
+
+    def test_creating_posts(self):
+        login = self.client.login(username = 'jungo', email='jungo@gmail.com', password='1234')
+        self.assertTrue(login)
+
+        url = reverse('posts:post_create')
+        image = SimpleUploadedFile("test.jpg", b"whatevercontents")
+        response = self.client.post(
+            url,
+            {"image":image, "caption":'test test'}
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsede(response, "posts/base.html")
+    
+    def test_post_posts_create_not_login(self):
+        url = reverse('posts:post_create')
+        image = SimpleUploadedFile("test.jpg", b"whatevercontents")
+        response = self.client.post(
+            url,
+            {"image":image, "caption":'test test'}
+        )
+
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsede(response, "users/main.html")
